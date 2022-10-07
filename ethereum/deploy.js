e1798dc9fd90fd984d1fdb9db054bec01621f05b
Nirmalya Saha <sahanirmalyo96@gmail.com>
2022-10-07 13:27:08 +0530

    Add Ethereum Sepolia Migration

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -4,7 +4,7 @@ const compiledFactory = require('./build/FundRaiserFactory.json');
 
 const provider = new HDWalletProvider(
   'prize verify carpet deposit game round burden cabin general boil topic world',
-  'https://rinkeby.infura.io/v3/27d0617b4c5a4f5aa18370f47ccd3028'
+  'https://sepolia.infura.io/v3/27d0617b4c5a4f5aa18370f47ccd3028'
 );
 const web3 = new Web3(provider);
 
@@ -21,4 +21,4 @@ const deploy = async () => {    //deployed script
   console.log('Contract deployed to', result.options.address);
 };
 deploy();
-//0xB8d641c59F3e56db87F26ACB9E271C4564e96F80
+//  '0xB8d641c59F3e56db87F26ACB9E271C4564e96F80'
--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import FundRaiserFactory from './build/FundRaiserFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(FundRaiserFactory.interface),
-  '0xB8d641c59F3e56db87F26ACB9E271C4564e96F80'
+  '0xA0BB0fb129ED293E94a6f80341a62D452B50791d'
 );
 
 export default instance;
